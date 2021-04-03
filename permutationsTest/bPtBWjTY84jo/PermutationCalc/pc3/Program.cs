ff8569daa8356a9919130f6431c35cb3961c379e
fdsc <fdsc@yandex.ru>
2021-04-03 22:23:16 +0300

    Добавил чуток исправленных расчётов статистики

--- permutationsTest/bPtBWjTY84jo/PermutationCalc/pc3/Program.cs
+++ permutationsTest/bPtBWjTY84jo/PermutationCalc/pc3/Program.cs
@@ -4,32 +4,20 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static factorial_library.Factorial;
 
 namespace pc2
 {
     class Program
     {
-        // Проверка формул для расчёта количества сочетаний с повторениями
+        // Распределяем 1 и 0 по массиву
         static void Main(string[] args)
         {
-            int n = 20;  // Длина гаммы
-            int b = 4;  // Количество вариантов, которые могут быть представлены в одном числе (4 - два бита)
-            int m = 1;  // Минимальное число встреченных вариантов каждого числа
-
-            // Расчёт по формуле
-            var f1 = factorial(n+b-1)           / factorial(n) / factorial(b-1);
-            var f2 = factorial(n - m*b + b - 1) / factorial(n - m*b) / factorial(b - 1);
-
-            Console.WriteLine(f1 + "\t" + f2);
-            File.WriteAllText("tmp-f1.txt", f1.ToString());
-            File.WriteAllText("tmp-f2.txt", f2.ToString());
-            File.WriteAllText("tmp-f2-f1.txt", (1_000_000*f1/f2).ToString());
-
+            int n = 3;  // Количество единиц для распределения
+            int b = 5;  // Длина массива битов
 
             int all = 0, allm = 0;
             int[] numbers = new int[b];
-            calc(numbers, ref all, ref allm, n, b, m);
+            calc(numbers, ref all, ref allm, n, b, 0);
 
             Console.WriteLine(all + "\t" + allm);
 
@@ -52,10 +40,10 @@ namespace pc2
 
             if (index >= b || s >= n)
             {
-                if (s < n)
-                    return;
-
                 all++;
+
+                if (s != n)
+                    return;
 
                 var sb = new StringBuilder(b*3);
                 for (int i = 0; i < b; i++)
@@ -68,24 +56,20 @@ namespace pc2
                 File.AppendAllText("permutations.txt", sb.ToString() + "\r\n");
 
                 // Это если последние элементы number равны 0 и мы их не учитываем.
-                if (index != b && m > 0)
+                /*if (index != b && m > 0)
                     return;
-
-                for (int i = 0; i < index; i++)
-                {
-                    if (numbers[i] < m)
-                        return;
-                }
+                */
 
                 allm++;
                 return;
             }
 
-            for (int i = 0; i <= n - s; i++)
+            // for (int i = 0; i <= n - s; i++)
             {
-                numbers[index] = i;
+                numbers[index] = 1;
                 calc(index+1, numbers, ref all, ref allm, n, b, m);
                 numbers[index] = 0;
+                calc(index+1, numbers, ref all, ref allm, n, b, m);
             }
         }
     }
---
+++ permutationsTest/bPtBWjTY84jo/PermutationCalc/pc4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static factorial_library.Factorial;
+
+namespace pc4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // n - длина гаммы
+            // k - количество вхождений в гамму некоторого числа
+            // b - количество возможных числел в гамме (при побайтовой гамме b = 256)
+            int b = 256;
+            int n = 4096;
+            double minP = 1e-6;
+
+            File.WriteAllText("P.txt", "");
+
+            double summP = 0;
+            for (int k = 0; k < n; k++)
+            {
+                // n!/k!/(n-k)! * 1/b^k*(1-1/b)^-(n-k)
+                var K = factorial(n)/factorial(k)/factorial(n-k);
+
+                var P  = ((double) K) * Math.Pow(b, -k) * Math.Pow(1-1.0/b, n-k);
+                summP += P;
+
+                if (1.0 - summP < minP)
+                    break;
+
+                var str = k.ToString("D2") + ":\t" + P.ToString("F6");
+                Console.WriteLine(str);
+                File.AppendAllText("P.txt", str + "\r\n");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(summP.ToString("F6"));
+            File.AppendAllText("P.txt", summP.ToString("F6") + "\r\n");
+
+            Console.ReadKey();
+        }
+    }
+}
